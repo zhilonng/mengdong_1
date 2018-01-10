aa5bca8143529d84d77c54a66c25b106a2460cec
Heaven <Heaven@macdeMacBook-Air.local>
2018-01-10 22:12:14 +0800

    2018/01/10
    index.js index.wxml index.wxss

--- HappyBonus/pages/index/index.js
+++ HappyBonus/pages/index/index.js
@@ -5,7 +5,14 @@ const app = getApp()
 Page({
   data: {
     houBaoStyle:1,
-    userInfo:[],
+    userInfo : [],
+    choseimg : '' ,
+    picRandName:'',
+    picUrl:'',
+    price:'',
+    uid:'',
+    text:'',
+    token : '',
     shuoming1:'小伙伴们说对口令就能获得随即打赏',
     shuoming2:'好友听完你说的话就能领取赏金',
     shuoming3:'好友念出你的答案就能领取赏金',
@@ -30,9 +37,25 @@ Page({
         console.log(user)
         that.setData({
           userInfo:user.userInfo,
+          choseimg:user.userInfo.avatarUrl,
         })
       }
     })
+    wx.request({
+      url: 'https://baby.mamid.cn/User/Upload/getToken', 
+      method: 'POST',
+      data: {},
+      header: { 
+        'content-type':'application/x-www-form-urlencoded'
+      },
+      success: function(res) {
+        console.log(res.data)
+        that.setData({
+        token:res.data,
+        })
+      }
+      })
+
   },
 
 
@@ -104,4 +127,81 @@ Page({
       answer:e.detail.value,
     })
   },
+  pubimg:function(){
+    var that=this;
+      wx.chooseImage({
+        count: 1, 
+        sizeType: ['compressed'], 
+        sourceType: ['album', 'camera'],
+        success: function(res){
+        that.setData({
+           choseimg:res.tempFilePaths[0],
+           picRandName:Math.random().toString(36).substr(2)
+        })
+        }
+      })    
+  },
+  tapshare:function(){
+    var that= this;
+    var upload = function() {
+      console.log('开始上传图片了')
+      wx.uploadFile({
+          url: 'https://up.qbox.me',
+          filePath: that.data.choseimg,
+          name: 'file',
+          formData: {
+              'key': that.data.picRandName ,
+              'token': that.data.token
+          },
+          success: function(result) {
+              console.log(result)
+              if(result.statusCode==200){
+                that.setData({
+                  picUrl:'http://p2bl4mkhf.bkt.clouddn.com/'+that.data.picRandName,
+                  uid:101074,
+                  price:9.99,
+                  text:'卖萌',
+                })
+                createOrder();
+              }
+          },
+          fail: function(error) {
+              console.log(error)
+          }
+      })
+    }
+   var createOrder=function(){
+        //开始图文入库
+      wx.request({
+        url: 'https://baby.mamid.cn/User/Order/createOrder',
+        method: 'POST',
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: {
+          picUrl:that.data.picUrl,
+          uid:that.data.uid,
+          price:that.data.price,
+          text:that.data.text,
+      },
+      //发布图片成功
+      success:function(res){
+      console.log(res)
+      if(res.statusCode==200){
+        wx.showToast({
+          title: '发布成功',
+          icon: 'success',
+          duration: 500
+        })
+        //跳转到分享页面
+      }
+      }
+    })
+    }
+    if(that.data.choseimg.length!=0){
+      upload();
+    }
+  }
+
+
 })
